cc5da5ca6987695b57e97d53b85e921af4a9a1da
sevbesau <seppe@sevbesau.xyz>
2022-08-06 12:02:20 +0200

    added health tracker beheviour

---
+++ Deckxquis/Assets/HealthTrackerBehaviour.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthTrackerBehaviour : MonoBehaviour
+{
+    public int _maxHealthLevel;
+    public int _maxDefenceLevel; 
+    private int _healthLevel;
+    private int _defenceLevel;
+    
+    public int HealthLevel { get => _healthLevel; }
+    public int DefenceLevel { get => _defenceLevel; }
+    public int MaxHealthLevel { set => _maxHealthLevel = value; }
+    public int MaxDefenceLevel { set => _maxDefenceLevel = value; }
+
+    public void takeDamage(int amount) {
+        if (_defenceLevel > 0) {
+            if (amount > _defenceLevel) {
+                _healthLevel -= amount - _defenceLevel;
+                _defenceLevel = 0;
+            } else {
+                _defenceLevel -= amount;
+            }
+        } else {
+            _healthLevel -= amount;
+        }
+    }
+    
+    public void heal(int amount) {
+        _healthLevel += amount;
+        _healthLevel = Mathf.min(_healthLevel, _maxHealthLevel);
+    }
+    
+    public void increaseDefence(int amount) {
+        _defenceLevel += amount;
+        _defenceLevel = Mathf.min(_defenceLevel, _maxDefenceLevel);
+    }
+}
