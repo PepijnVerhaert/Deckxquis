5a4c46cbcdb7615b1f60085784dd4adff0acd274
sevbesau <seppe@sevbesau.xyz>
2022-08-06 11:42:42 +0200

    fix compilation errors

--- Deckxquis/Assets/Scripts/EnergyTrackerBehaviour.cs
+++ Deckxquis/Assets/Scripts/EnergyTrackerBehaviour.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class EnergyTrackerBehaviour : MonoBehaviour
 {
-    public int _energyLevel { get; }
-    
-    public void decrementEnergyLevel(int amount=1) {
+    private int _energyLevel;
+
+    public int EnergyLevel { get => _energyLevel; }
+
+    public void useEnergy(int amount=1) {
         _energyLevel -= amount;
     }
 }
