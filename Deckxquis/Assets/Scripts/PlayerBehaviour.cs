7a28adce3c9af3e437c90457c751dd6283a6ccd8
sevbesau <seppe@sevbesau.xyz>
2022-08-07 01:23:31 +0200

    fix isPicking

--- Deckxquis/Assets/Scripts/PlayerBehaviour.cs
+++ Deckxquis/Assets/Scripts/PlayerBehaviour.cs
@@ -36,6 +36,7 @@ public class PlayerBehaviour : MonoBehaviour
         }  else  {
             _pickingPart.setCardProperties(cardProperties);
         }
+        _isPicking = false;
     }
     
     public void Start() {
